cae1ba0d37f079681f34fcd4d1e0092edb9b52df
Aleksey Pakhomchik <aleksey.pakhomchik@gmail.com>
2021-10-24 02:48:40 +0300

    Moved models into models.py

--- quazik/tsp/views.py
+++ quazik/tsp/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import generic
 from django.http import HttpResponse
-from django.contrib import messages
-from quazik.settings import GOOGLE_TOKEN
 
-from typing import List
-import requests
+from tsp.models import Marker, return_distance_by_google_api
 
 
 class Index(generic.View):
@@ -25,51 +22,6 @@ def number_of_cities(request):
 
     dct = {'cities': list(range(cities))}
     return render(request, 'tsp/choose_cities.html', dct)
-
-
-class Marker:
-    def __init__(self, name, lat, lng):
-        self.name = name
-        self.lat = lat
-        self.lng = lng
-
-
-class Distance:
-    def __init__(self, origin: Marker, destination: Marker, distance: float):
-        self.origin = origin
-        self.destination = destination
-        self.distance = distance
-
-    def __str__(self):
-        return ','.join([self.origin.name, self.destination.name, str(self.distance)])
-
-
-def return_distance_by_google_api(markers: List[Marker]) -> List[Distance]:
-    url = 'https://maps.googleapis.com/maps/api/distancematrix/json'
-    origins = r'%7C'.join([
-        f"{marker.lat}%2C{marker.lng}"
-        for marker in markers
-    ])
-    destinations = r'%7C'.join([
-        f"{marker.lat}%2C{marker.lng}"
-        for marker in markers
-    ])
-    response = requests.get(f"{url}?origins={origins}&destinations={destinations}&key={GOOGLE_TOKEN}")
-
-    print(f"Status code: {response.status_code}")
-    print(f"Reason: {response.reason}")
-    print(response.json())
-
-    if response.status_code != 200:
-        raise Exception(response.reason)
-
-    distances = []
-    for i, row in enumerate(response.json()['rows']):
-        for j, value in enumerate(row['elements']):
-            print(value)
-            distances.append(Distance(markers[i], markers[j], value["duration"]['value']))
-
-    return distances
 
 
 class SolveTSP(generic.View):
